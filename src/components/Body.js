0504ae1b0a7119922f341490c98f4c162c2c4bf5
Gulshan-Codenet-Softwares <gulshan.codenet@gmail.com>
2025-01-05 18:38:24 +0530

    .

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,20 @@
 import ResturantCard from "./ResturantCard"
+import { useState } from "react"
 import resList from "../utils/mockData"
 const Body = ()=>{
+    const [restaurantList,setrestaurantList] = useState(resList)
     return (
         <div className="Body">
-            <div className="search">Search</div>
+       
+            <div className="filter">
+                <button className="filter-btn" onClick={()=>{
+                const a = resList.filter(res=>res.data.avgRating>4)
+                setrestaurantList(a)
+                 }}>Top Rated Resturants</button>
+            </div>
             <div className="res-container">
             {
-               resList.map((resturant)=>{
+                restaurantList.map((resturant)=>{
                 return<ResturantCard key={resturant.data.id}  resData = {resturant}/>
                }) 
             }
